c9b66831b270731dfcfc2271385526b53fe1f896
Sam Fullman <sfullman@presidio.com>
2019-03-18 07:58:13 -0500

    rework compile process so HTML doesn't have to be in single quotes to write it.  We'll need it to redo/add flexbox etc.

--- src/compile.php
+++ src/compile.php
@@ -1,7 +1,7 @@
 <?php
 // compile order:
 $files = [
-    'html-assets.js',
+    '_AVAYA_HTML_.html',
     'libs/xmlToJSON.js',
     'libs/AvayaClientServices.min.js',
     'avayaGlobal.js',
@@ -15,6 +15,7 @@ $files = [
     'oceanaCoreData.js',
     'customerJourneyCommon.js',
 ];
+$stringize = ['_AVAYA_HTML_.html'];
 $str = '';
 $lines = 0;
 $here = __FILE__;
@@ -23,7 +24,27 @@ foreach($files as $i=>$file){
     $str .= '/* ---- File (' . ($i+1) . '): ' . $file . '; compiled ' . date('Y-m-d H:i:s') . ' by /src/compile.php ---- */' . "\n";
     $a = file($file);
     $lines += count($a);
-    $str .= implode('', $a);
+	if(in_array($file, $stringize)){
+		$var = explode('.', $file);
+		$var = $var[0];
+		$str .= 'var '. $var . ' = ' . "\n";
+		foreach($a as $n => $line){
+			$apos = false;
+			$line = str_replace("\n" , '', $line);
+			$line = str_replace("\r" , '', $line);
+			for($i = 0; $i < strlen($line); $i++){
+				if(!$apos && !preg_match('/\s/', $line{$i})){
+					$str .= '\'';
+					$apos = true;
+				}
+				$str .= $line{$i};
+			}
+			if(!$apos)$str .= '\'';
+			$str .= ($n === count($a) - 1 ? '\';' : '\' + ') . "\n";
+		}
+	}else{
+		$str .= implode('', $a);
+	}
     $str .= "\n\n";
 
 
